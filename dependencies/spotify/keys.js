3804bb3f3aabcdb4ec0cb0ff3218731225fde071
Jackson Oppenheim <jacksonopp@gmail.com>
2019-09-12 14:26:03 -0500

    fixed .cli issue

--- dependencies/spotify/keys.js
+++ dependencies/spotify/keys.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 console.log("this is loaded");
 
+
 exports.spotify = {
-    id: "a52d88d4ed084733b8fe919fdbd23656",
-    secret: "c7afcc5291674aba99c3605f6eb063f7"
-}
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
